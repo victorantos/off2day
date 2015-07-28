922187b12e9b70c6d036d4a1bd0f54558a598e37
Victor Antofica <hello@victorantos.com>
2015-07-28 16:27:08 +0100

    initial commit, includes a React working script, Hello.jsx

---
+++ src/Off2day/Startup.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Diagnostics;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+using Microsoft.Framework.Logging.Console;
+using Microsoft.Framework.Runtime;
+using React.AspNet;
+
+namespace Off2day
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            // Setup configuration sources.
+            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //ReactServiceCollectionExtensions.AddReact(services);
+           services.AddReact();
+
+            // Add MVC services to the services container.
+            services.AddMvc();
+
+            // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
+            // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
+            // services.AddWebApiConventions();
+        }
+
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.MinimumLevel = LogLevel.Information;
+            loggerFactory.AddConsole();
+
+            // Configure the HTTP request pipeline.
+
+            // Add the following to the request pipeline only in development environment.
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseErrorPage(ErrorPageOptions.ShowAll);
+            }
+            else
+            {
+                // Add Error handling middleware which catches all application specific errors and
+                // send the request to the following path or controller action.
+                app.UseErrorHandler("/Home/Error");
+            }
+
+             //app.UseRequestServices();
+            app.UseReact(
+           config =>
+           {
+               // ES6 features are enabled by default. Uncomment the below line to disable them.
+               // See http://reactjs.net/guides/es6.html for more information.
+               //config.SetUseHarmony(false);
+               // Uncomment the below line if you are using Flow
+               // See http://reactjs.net/guides/flow.html for more information.
+               //config.SetStripTypes(true);
+               // If you want to use server-side rendering of React components,
+               // add all the necessary JavaScript files here. This includes
+               // your components as well as all of their dependencies.
+               // See http://reactjs.net/ for more information. Example:
+               config
+                .AddScript("~/React/Hello.jsx");
+               //.AddScript("~/Scripts/Second.jsx");
+           });
+
+            // Add static files to the request pipeline.
+            app.UseStaticFiles();
+
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+        }
+    }
+}
---
+++ src/Off2day/wwwroot/React/Hello.jsx
@@ -0,0 +1,9 @@
+﻿var Hello = React.createClass({
+    render: function () {
+        return (<div>so it works fne</div>);
+    }
+});
+
+React.render( <Hello />,
+            document.getElementById('example')
+          );
